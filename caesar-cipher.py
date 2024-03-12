834a2af992367a0b7037e569ceb68ee137e09170
saurxbh <36542272+saurxbh@users.noreply.github.com>
2024-03-12 01:04:39 -0400

    Encrypt/decrypt message and display the result

--- caesar-cipher.py
+++ caesar-cipher.py
@@ -42,8 +42,44 @@ while True: # Keep asking until the user enters a valid key
         break
 
 # Let the user enter the message to encrypt/decrypt
-print('Enter the message to {}'.format(mode))
+print('Enter the message to {}.'.format(mode))
 message = input('> ').upper()
+
+# Stores the encrypted/decrypted form of the message
+translated = ''
+
+# Encrypt/decrypt each symbol in the message
+for symbol in message:
+    if symbol in SYMBOLS:
+        # Get the encrypted or decrypted numbers for this symbol
+        num = SYMBOLS.find(symbol) # Get the number of the symbol
+        if mode == 'encrypt':
+            num += key
+        elif mode == 'decrypt':
+            num -= key
+
+        # Handle wrap-around if num is larger than length of SYMBOLS or less than 0
+        if num >= len(SYMBOLS):
+            num -= len(SYMBOLS)
+        if num < 0:
+            num += len(SYMBOLS)
+
+        # Add the encrypted/decrypted number's symbol to translated
+        translated = translated + SYMBOLS[num]
+
+    else:
+        # Just add the symbol without encrypting/decrypting
+        translated = translated + symbol
+
+# Display the encrypted/decrypted message
+print(translated)
+
+try:
+    pyperclip.copy(translated)
+    print('Full {}ed text copied to clipboard.'.format(mode))
+except ImportError:
+    pass
+
 
 
 
